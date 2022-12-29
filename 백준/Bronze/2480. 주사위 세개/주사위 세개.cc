ec066b477edeaff4294995db75e34940fcbb382c
HayunKwon <97526153+HayunKwon@users.noreply.github.com>
2022-12-29 15:12:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int prize;
+
+	if (a == b && b== c)
+		prize = 10000 + a * 1000;
+	else if (a == b || a == c)
+		prize = 1000 + a * 100;
+	else if (b == c)
+		prize = 1000 + b * 100;
+	else
+		prize = max(max(a, b),c) * 100;
+
+
+	cout << prize;
+	return 0;
+}
