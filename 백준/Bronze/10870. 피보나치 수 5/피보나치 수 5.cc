82650d58ebe79f3c85f1e790a9e00781c0873325
HayunKwon <97526153+HayunKwon@users.noreply.github.com>
2022-12-29 15:18:01 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int num){
+    if(num == 0)
+        return 0;
+    else if(num == 1)
+        return 1;
+    else{
+        return fibo(num-2) + fibo(num-1);
+    }
+}
+
+int main(){
+    int num;
+    cin >> num;
+    cout << fibo(num) << '\n';
+}
