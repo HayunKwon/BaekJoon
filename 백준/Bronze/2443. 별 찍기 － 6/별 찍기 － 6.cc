c4652a2a84cf5e5b48a29249bf45c8beebdd998c
HayunKwon <97526153+HayunKwon@users.noreply.github.com>
2023-01-10 14:30:17 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
- 
+
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
- 
+	
 	int N;
 	cin >> N;
- 
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < i; j++)
 			cout << ' ';
@@ -16,6 +14,6 @@ int main(void)
 			cout << '*';
 		cout << '\n';
 	}
- 
+
 	return 0;
 }
