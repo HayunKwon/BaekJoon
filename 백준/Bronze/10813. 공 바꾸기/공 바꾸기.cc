c0f32e11f984cf5802e6f3aa02f249c395d784f0
HayunKwon <97526153+HayunKwon@users.noreply.github.com>
2023-02-24 17:00:25 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m, a, b;
+    cin >> n >> m;
+    int *bas = new int[n+1];
+    for (int i = 1; i <= n; i++)
+        bas[i] = i;
+    while (m--)
+    {
+        cin >> a >> b;
+        swap(bas[a], bas[b]);
+    }
+    for (int i = 1; i <= n; i++)
+        cout << bas[i] << ' ';
+}
